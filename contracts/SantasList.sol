7279b06ae1fd8ac3173af1b5e42d2cd243b96ece
Vincent Chee <0xdavinchee@gmail.com>
2021-06-04 20:29:24 +0800

    tests implemented
    - we will base the withdraw fees off balance rather than tracking it manually
    - getWithdrawableAmount function lets users know how much they are allowed to withdraw
    - readme updated

--- contracts/SantasList.sol
+++ contracts/SantasList.sol
@@ -1,11 +1,16 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract SantasList {
+    using SafeMath for uint256;
     struct NiceLister {
         uint256 feesWithdrawn;
         bool valid;
     }
+
+    address[] niceListAddresses;
 
     mapping(address => NiceLister) public niceList;
     mapping(address => bool) public naughtyList;
@@ -18,6 +23,7 @@ contract SantasList {
 
     event NewNiceLister(address niceLister, uint256 feePaid);
     event NewNaughtyLister(address naughtyLister);
+    event WithdrawFees(address withdrawer, uint256 amount);
 
     constructor(address[] memory _naughtyList) {
         owner = msg.sender;
@@ -66,22 +72,69 @@ contract SantasList {
 
         niceList[msg.sender] = NiceLister(0, true);
         naughtyList[msg.sender] = false;
-        numNiceListers += 1;
+        numNiceListers = numNiceListers.add(1);
+        feeAmount = 0.01 ether * (numNiceListers.add(1))**2;
+        niceListAddresses.push(msg.sender);
         totalFeesAccrued += msg.value;
-        feeAmount = 0.01 ether * (numNiceListers + 1)**2;
 
         emit NewNiceLister(msg.sender, msg.value);
     }
 
     function withdrawShareOfFees() external payable {
         require(niceList[msg.sender].valid, "You aren't on the nice list.");
-        uint256 withdrawableAmount =
-            totalFeesAccrued /
-                numNiceListers -
-                niceList[msg.sender].feesWithdrawn;
-        require(withdrawableAmount > 0, "You have withdrawn your share.");
+        uint256 withdrawableAmount = getWithdrawableAmount();
+        require(
+            withdrawableAmount > 0,
+            "You have already withdrawn your share."
+        );
+        niceList[msg.sender].feesWithdrawn = niceList[msg.sender]
+            .feesWithdrawn
+            .add(withdrawableAmount);
         (bool sent, ) = msg.sender.call{value: withdrawableAmount}("");
         require(sent, "Unable to withdraw funds.");
+        emit WithdrawFees(msg.sender, withdrawableAmount);
+    }
+
+    function ownerWithdrawBounty() external payable onlyOwner {
+        uint256 withdrawableAmount = getOwnerWithdrawableAmount();
+        require(withdrawableAmount > 0, "You have nothing to withdraw.");
+        (bool sent, ) = msg.sender.call{value: withdrawableAmount}("");
+        require(sent, "Unable to withdraw funds.");
+        emit WithdrawFees(msg.sender, withdrawableAmount);
+    }
+
+    function getWithdrawableAmount() public view returns (uint256) {
+        return _getWithdrawableAmount(msg.sender);
+    }
+
+    function getOwnerWithdrawableAmount() public view returns (uint256) {
+        uint256 ownerAmount = totalFeesAccrued;
+        for (uint256 i = 0; i < niceListAddresses.length; i++) {
+            uint256 niceListWithdrawableAmount =
+                _getWithdrawableAmount(niceListAddresses[i]);
+            if (niceListWithdrawableAmount > 0) {
+                ownerAmount = 0;
+                break;
+            }
+            ownerAmount = ownerAmount.sub(
+                niceList[niceListAddresses[i]].feesWithdrawn
+            );
+        }
+        return ownerAmount;
+    }
+
+    function _getWithdrawableAmount(address _address)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 averageWithdrawalAmount =
+            address(this).balance / numNiceListers;
+        return
+            address(this).balance == 0 ||
+                averageWithdrawalAmount < niceList[_address].feesWithdrawn
+                ? 0
+                : averageWithdrawalAmount.sub(niceList[_address].feesWithdrawn);
     }
 
     receive() external payable {}
--- test/santas-list-test.test.ts
+++ test/santas-list-test.test.ts
@@ -1,6 +1,16 @@
 import { expect } from "./chai-setup";
 import { SantasList } from "../typechain";
-import { ethers, deployments } from "hardhat";
+import {
+  ethers,
+  deployments,
+  getNamedAccounts,
+  getUnnamedAccounts,
+} from "hardhat";
+import { setupUser, setupUsers } from "./utils";
+
+const getEth = (amount: string) => ethers.utils.parseEther(amount);
+const getEthFeeAmount = (numNiceListers: number) =>
+  getEth((0.01 * (numNiceListers + 1) ** 2).toString());
 
 const setup = async () => {
   await deployments.fixture(["SantasList"]);
@@ -8,15 +18,192 @@ const setup = async () => {
     SantasList: (await ethers.getContract("SantasList")) as SantasList,
   };
 
-  return { ...contracts };
+  const { deployer } = await getNamedAccounts();
+  const participants = await getUnnamedAccounts();
+
+  return {
+    deployer: await setupUser(deployer, contracts),
+    participants: await setupUsers(participants, contracts),
+    ...contracts,
+  };
 };
 
-describe("SantasList", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const { SantasList } = await setup();
-    expect(await SantasList.greet()).to.equal("Hello Hardhat!");
+describe("SantasList Tests", () => {
+  describe("Initialization", () => {
+    it("Should initialize contract with empty variables", async () => {
+      const { participants, SantasList } = await setup();
+      expect(await SantasList.feeAmount()).to.equal(getEth("0.01"));
+      expect(await SantasList.numNiceListers()).to.equal(0);
+      expect(await SantasList.naughtyList(participants[0].address)).to.equal(
+        true
+      );
+      expect(await SantasList.naughtyList(participants[1].address)).to.equal(
+        true
+      );
+      expect(await SantasList.naughtyList(participants[2].address)).to.equal(
+        false
+      );
+    });
+  });
 
-    await SantasList.setGreeting("Hola, mundo!");
-    expect(await SantasList.greet()).to.equal("Hola, mundo!");
+  describe("Naughty List Tests", () => {
+    it("Should be able to add new participants to naughty list.", async () => {
+      const { participants, SantasList } = await setup();
+      await expect(SantasList.addToNaughtyList(participants[2].address))
+        .to.emit(SantasList, "NewNaughtyLister")
+        .withArgs(participants[2].address);
+      await expect(SantasList.addToNaughtyList(participants[3].address))
+        .to.emit(SantasList, "NewNaughtyLister")
+        .withArgs(participants[3].address);
+      await expect(SantasList.addToNaughtyList(participants[4].address))
+        .to.emit(SantasList, "NewNaughtyLister")
+        .withArgs(participants[4].address);
+    });
+
+    it("Should properly reject invalid naughty list adds.", async () => {
+      const { deployer, participants, SantasList } = await setup();
+
+      await expect(
+        participants[0].SantasList.addToNaughtyList(participants[3].address)
+      ).to.be.revertedWith("You are not the list owner.");
+
+      await expect(
+        SantasList.addToNaughtyList(participants[0].address)
+      ).to.be.revertedWith("This address is already on the naughty list.");
+      const feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await participants[0].SantasList.joinNiceList({
+        value: feeAmount,
+      });
+      await expect(
+        SantasList.addToNaughtyList(participants[0].address)
+      ).to.be.revertedWith("This address is on the nice list.");
+
+      await expect(
+        SantasList.addToNaughtyList(deployer.address)
+      ).to.be.revertedWith(
+        "You are not allowed to add yourself to the naughty list."
+      );
+    });
+  });
+
+  describe("Nice List Tests", () => {
+    it("Should be able to join the nice list.", async () => {
+      const { participants, SantasList } = await setup();
+      let feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await expect(
+        participants[0].SantasList.joinNiceList({ value: feeAmount })
+      )
+        .to.emit(SantasList, "NewNiceLister")
+        .withArgs(participants[0].address, feeAmount);
+      expect(await SantasList.numNiceListers()).to.equal(1);
+      expect(await SantasList.naughtyList(participants[0].address)).to.equal(
+        false
+      );
+      feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await expect(
+        participants[1].SantasList.joinNiceList({ value: feeAmount })
+      )
+        .to.emit(SantasList, "NewNiceLister")
+        .withArgs(participants[1].address, feeAmount);
+    });
+
+    it("Should properly reject invalid nice list adds.", async () => {
+      const { participants, SantasList } = await setup();
+
+      await expect(
+        SantasList.joinNiceList({ value: getEth("0.01") })
+      ).to.be.revertedWith("You aren't on the naughty list.");
+
+      await participants[0].SantasList.joinNiceList({ value: getEth("0.01") });
+      await expect(
+        participants[0].SantasList.joinNiceList({ value: getEth("0.01") })
+      ).to.be.revertedWith("You are already on the nice list.");
+
+      await expect(
+        participants[1].SantasList.joinNiceList({ value: getEth("0.01") })
+      ).to.be.revertedWith(
+        "You must pay the fee amount to get on the nice list."
+      );
+    });
+  });
+
+  describe("Withdraw Tests", () => {
+    it("Should be able to withdraw.", async () => {
+      const { participants, SantasList } = await setup();
+      let feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await participants[0].SantasList.joinNiceList({ value: feeAmount });
+      await expect(participants[0].SantasList.withdrawShareOfFees())
+        .to.emit(SantasList, "WithdrawFees")
+        .withArgs(participants[0].address, feeAmount);
+
+      feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await participants[1].SantasList.joinNiceList({ value: feeAmount });
+      await expect(participants[0].SantasList.withdrawShareOfFees())
+        .to.emit(SantasList, "WithdrawFees")
+        .withArgs(participants[0].address, getEth("0.01"));
+
+      await expect(participants[1].SantasList.withdrawShareOfFees())
+        .to.emit(SantasList, "WithdrawFees")
+        .withArgs(participants[1].address, getEth("0.015"));
+    });
+
+    it("Should properly reject invalid withdrawals.", async () => {
+      const { participants, SantasList } = await setup();
+
+      let feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await expect(SantasList.withdrawShareOfFees()).to.be.revertedWith(
+        "You aren't on the nice list."
+      );
+
+      await participants[0].SantasList.joinNiceList({ value: feeAmount });
+      await participants[0].SantasList.withdrawShareOfFees();
+      await expect(
+        participants[0].SantasList.withdrawShareOfFees()
+      ).to.be.revertedWith("You have already withdrawn your share.");
+    });
+
+    it("Should properly calculate owner withdrawal amount.", async () => {
+      const { deployer, participants, SantasList } = await setup();
+
+      let feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+
+      await expect(SantasList.ownerWithdrawBounty()).to.be.revertedWith(
+        "You have nothing to withdraw."
+      );
+
+      await participants[0].SantasList.joinNiceList({ value: feeAmount }); // 0.01 in the contract
+
+      await participants[0].SantasList.withdrawShareOfFees(); // 0.01 withdrawn
+
+      // 0 in the contract now
+
+      feeAmount = getEthFeeAmount(
+        (await SantasList.numNiceListers()).toNumber()
+      );
+      await participants[1].SantasList.joinNiceList({ value: feeAmount }); // 0.04 in the contract now
+
+      await participants[0].SantasList.withdrawShareOfFees(); // 0.04 / 2 - 0.01 = 0.01 withdrawn
+      await participants[1].SantasList.withdrawShareOfFees(); // 0.03 / 2 = 0.015 withdrawn
+      expect(await SantasList.getOwnerWithdrawableAmount()).to.equal(
+        getEth("0.015")
+      );
+      await expect(SantasList.ownerWithdrawBounty())
+        .to.emit(SantasList, "WithdrawFees")
+        .withArgs(deployer.address, getEth("0.015"));
+    });
   });
 });
